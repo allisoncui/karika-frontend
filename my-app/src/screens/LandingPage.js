ef46624374e820044a4fc5529d350c285777eb74
alyucu6 <allisonycui@gmail.com>
2024-04-05 18:25:43 -0400

    landing page

--- my-app/src/screens/LandingPage.js
+++ my-app/src/screens/LandingPage.js
@@ -3,6 +3,7 @@ import "./LandingPage.css";
 import dummy_photo from "../assets/dummy_photo.png";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import karikaLogo from "../karika-logo-cropped.png";
 
 function LandingPage() {
   const [patient, setPatient] = useState(false);
@@ -24,10 +25,19 @@ function LandingPage() {
   };
   return (
     <div className="landing_page_outer_container">
-      <div className="landing_page_heading">My Health Decision</div>
-      <div className="landing_page_subheading">
-        Picking the Best Health Options
+      <div className="heading-container">
+        <div className="image-container">
+          <img src={karikaLogo} alt="Karika Republic logo" className="logo_img" />
+        </div>
+        <div className="text-container">
+          <div className="landing_page_heading">KARIKA</div>
+          <div className="landing_page_heading2">REPUBLIC</div>
+        </div>
       </div>
+      <div className="landing_page_subheading">
+        Learn how to network and build a personal brand to grow your business & accelerate your career!
+      </div>
+
       <div className="landing_page_button_container">
         <div
           className={
@@ -38,7 +48,7 @@ function LandingPage() {
           }
           onClick={patientClick}
         >
-          I am a patient
+          I have an account
         </div>
         <div
           className={
@@ -49,7 +59,7 @@ function LandingPage() {
           }
           onClick={providerClick}
         >
-          I am a provider
+          Sign Up
         </div>
       </div>
       <div
@@ -61,7 +71,7 @@ function LandingPage() {
       >
         Next
       </div>
-    </div>
+    </div >
   );
 }
 
